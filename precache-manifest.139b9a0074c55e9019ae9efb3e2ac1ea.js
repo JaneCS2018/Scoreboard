079ed1206a20058a8e570575b89be811a4649ad5
Jane <xiangliu2011@gmail.com>
2020-09-30 17:17:23 -0700

    Updates

---
+++ precache-manifest.139b9a0074c55e9019ae9efb3e2ac1ea.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36c94c0658919105f84b25b722c17ea8",
+    "url": "/Scoreboard/index.html"
+  },
+  {
+    "revision": "ead7414fb5a9d62330a0",
+    "url": "/Scoreboard/static/css/2.8aa5a7f8.chunk.css"
+  },
+  {
+    "revision": "a5bb576e3e93ea5b480a",
+    "url": "/Scoreboard/static/css/main.f3015ef4.chunk.css"
+  },
+  {
+    "revision": "ead7414fb5a9d62330a0",
+    "url": "/Scoreboard/static/js/2.69193f42.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Scoreboard/static/js/2.69193f42.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a5bb576e3e93ea5b480a",
+    "url": "/Scoreboard/static/js/main.e1b41937.chunk.js"
+  },
+  {
+    "revision": "d98cd06f730ce677821a",
+    "url": "/Scoreboard/static/js/runtime-main.d153992b.js"
+  }
+]);
